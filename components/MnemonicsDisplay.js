409920b07cb14f13eed3195bcf3a3efe80871f99
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-09-17 22:46:57 -0400

    styling fixes

--- components/MnemonicsDisplay.js
+++ components/MnemonicsDisplay.js
@@ -1,8 +1,14 @@
 export default function MnemonicsDisplay({ mnemonicPhrase, seed }) {
   return (
     <div className="text-black text-[15px] break-all p-[25px]">
-        <p>{mnemonicPhrase}</p>
-        <p>{seed}</p>
+        <div className="flex">
+          {
+            mnemonicPhrase ? mnemonicPhrase.split(' ').map((mnemonic, index) => {
+              return <div id={index} className="p-[5px] border rounded-lg ml-[5px] bg-gray-500 text-white">{mnemonic}</div>
+            })  : ''
+          }
+        </div>
+        <div className="mt-[10px]"><span className="font-bold">Seed in hex:</span> {seed}</div>
     </div>
   );
 }
